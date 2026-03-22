beb140bf60176424089d9764858e59f41a8d234d
Jahronimo <anextvid@gmail.com>
2026-03-22 18:22:18 +0000

    Created v2.html entirely bypassing rigid HTML cache

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: './', // Allows relative paths, preventing broken static assets on GitHub Pages or anywhere else
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        v2: resolve(__dirname, 'v2.html')
+      }
+    }
+  }
 });
